6abc98cf46c43a49961c2eaf37f6924abc519a19
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2025-06-19 00:29:58 +0600

    mongoDB-connected

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
 // Ia2Dfc5jCd9KgoG8
 // emajohn_admin
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.thvamxq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
